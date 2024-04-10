86d992a2de3b10a8676fd3fbf310cb12699b575d
sunnat <sunnattoxtaqulov4444@gmail.com>
2024-04-10 18:18:31 +0300

    router add

--- sfood/src/App.jsx
+++ sfood/src/App.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import HomePages from './Pages/HomePages/HomePages'
 import './App.css'
-import {Route ,Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Fillial from './Pages/Fillial/Fillial';
 import Info from './Pages/Info/Info';
 import Contact from './Pages/Contact/Contact';
 const App = () => {
   return (
     <div>
-      <Routes>
+      <Router>
         <Route path="/" component={<HomePages/>}/>
         <Route path="/fillial" component={<Fillial/>}/>
         <Route path="/info" component={<Info/>}/>
         <Route path="/contact" component={<Contact/>}/>
-      </Routes>
+      </Router>
 
     </div>
   )
