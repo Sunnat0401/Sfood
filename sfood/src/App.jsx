48b553d50dd12a9c05f09c4377fad4f3a329dfff
sunnat <sunnattoxtaqulov4444@gmail.com>
2024-04-10 18:21:13 +0300

    clear

--- sfood/src/App.jsx
+++ sfood/src/App.jsx
@@ -1,21 +1,19 @@
 import React from 'react'
 import HomePages from './Pages/HomePages/HomePages'
 import './App.css'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import {   Routes, Route } from 'react-router-dom';
 import Fillial from './Pages/Fillial/Fillial';
 import Info from './Pages/Info/Info';
 import Contact from './Pages/Contact/Contact';
 const App = () => {
   return (
     <div>
-      <Router>
-        <Switch>
+      <Routes>
         <Route path="/" component={<HomePages/>}/>
         <Route path="/fillial" component={<Fillial/>}/>
         <Route path="/info" component={<Info/>}/>
         <Route path="/contact" component={<Contact/>}/>
-        </Switch>
-      </Router>
+      </Routes>
 
     </div>
   )
